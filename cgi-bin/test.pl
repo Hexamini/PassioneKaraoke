231ca7bc3738eb66ed4d0aed1f9aab10fedcc42e
korut94 <andreamnt94@gmail.com>
2015-11-18 16:05:19 +0100

    Formazione di una pagina con l'oggetto Page e Table

--- cgi-bin/test.pl
+++ cgi-bin/test.pl
@@ -1,10 +1,13 @@
 #!/usr/bin/perl
-use lib "cgi-bin"; #miei packages
+use lib "cgi-bin";
 
 use Object::Table;
+use Object::Page;
 
-print Table::getHTML( "Andrea", 
-		      "Mantovani", 
-		      "17 settembre 1994", 
-		      "+39 3466936174");
+$table = Table::getHTML( "Andrea", 
+			    "Mantovani", 
+			    "17 settembre 1994", 
+		            "+39 3466936174" );
+
+print Page::getHTML( $table );
 exit;

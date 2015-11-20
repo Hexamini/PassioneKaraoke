ed5f950edb8ba104d937b04991dd8797136271ca
Andrea Mantovani <andreamnt94@gmail.com>
2015-11-20 15:23:41 +0100

    Parametri passati per hash Keyword necessarie -nome = nome persona -cognome = cognome persona -data = data di nascita -numero = numero telefonico

--- cgi-bin/Object/Table.pm
+++ cgi-bin/Object/Table.pm
@@ -12,15 +12,10 @@ Parametri
 =cut
 sub getHTML
 {
-    my $struct = "data/table.html";
-
-    my %values;
-    $values{'nome'} = $_[0];
-    $values{'cognome'} = $_[1];
-    $values{'data'} = $_[2];
-    $values{'numero'} = $_[3];
+    my $struct = "table.html";
+    my ( $args ) = @_;
     
-    return ParserHTML::parsing( $struct, %values );
+    return ParserHTML::parsing( { filename => $struct, values => $args, } );
 }
 
 1;

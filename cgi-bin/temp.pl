9f5d552e6dfde7f08017fc4ff04f449df82315ef
korut94 <andreamnt94@gmail.com>
2015-11-20 13:33:48 +0100

    Uso del solo package ParserHTML

--- cgi-bin/temp.pl
+++ cgi-bin/temp.pl
@@ -11,25 +11,15 @@ use Object::Utility::ParserHTML;
 
 my $cgi = new CGI;
 print $cgi->header();
-=begin
-my $output = '';
 
-my $tt = Template->new({
-    RELATIVE => 1,
-    INCLUDE_PATH => "../data/views",
-    OUTPUT => \$output,
-});
+    my $values = {
+	nome => 'Andrea',
+	cognome => 'Mantovani',
+	data => '17 settembre 1994',
+	numero => '+393406936174',
+    };
 
-my $values = {
-    nome => 'Andrea',
-    cognome => 'Mantovani',
-    data => '17 settembre 1994',
-    numero => '+393406936174',
-};
 
-$tt->process( 'table.html', $values ) || die $tt->error();
-#print redirect( -url => '../public_html/table.html' );
-=cut
-my $object = ParserHTML::parsing();
+my $object = ParserHTML::parsing( { filename => 'table.html', values => $values, } );
 print $object . "\n";
 

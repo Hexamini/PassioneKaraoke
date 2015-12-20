32379fa70d13631e199198eea65aa7783c9093c0
korut94 <andreamnt94@gmail.com>
2015-12-20 20:27:32 +0100

    Gestione identificativo articolo non trovato Redirezione alla pagina 404. URL del server nella sezione di 1070258.

--- cgi-bin/Page/ArticlePage.pm
+++ cgi-bin/Page/ArticlePage.pm
@@ -1,5 +1,6 @@
 use lib "cgi-bin";
 use strict;
+use CGI;
 use XML::LibXML;
 
 use Page::Object::Article;
@@ -12,9 +13,10 @@ my $file = '../data/database/articleList';
 sub get
 {
     my( $parser, @pairs ) = @_;
+    
     my ( $id ) = ( ( shift @pairs ) =~ /=((\w|\d)+)$/ );
-
     my $doc = ParserXML::getDoc( $parser, $file );
+    
     $article = $doc->findnodes( "//article[\@id=$id]" )->get_node( 1 );
 
     if( $article )
@@ -27,7 +29,9 @@ sub get
     }
     else
     {
-	#redirect 404 page
+	#redirect to 404 page
+	my $cgi = CGI->new;
+	$cgi->redirect( 'tecnologie-web.studenti.math.unipd.it/~amantova/cgi-bin/r.cgi' ); 
     }
 }
 

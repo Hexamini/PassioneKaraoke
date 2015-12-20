c23f5cbac38c5c04618c334f8ce5c1085f637a13
korut94 <andreamnt94@gmail.com>
2015-12-20 17:02:31 +0100

    Intterrogazione database per ottenere le informazioni

--- cgi-bin/Page/ArticlesPage.pm
+++ cgi-bin/Page/ArticlesPage.pm
@@ -4,19 +4,29 @@ use XML::LibXML;
 
 use Page::Object::Articles;
 use Page::Object::ArticleList;
+use Page::Object::Base::ParserXML;
 
 package ArticlesPage;
 
-my $file = 'articleList.xml';
+my $file = '../data/database/articlelist.xml';
 
 sub get
 {
     my ( $parser ) = @_;
+    my $doc = ParserXML::getDoc( $parser, $file );
 
-    my $article1 = ArticleList::get( 'Adamo', 'Coglie la frutta' );
-    my $article2 = ArticleList::get( 'Ed Eva', 'Lo incula' );
+    my @nodes = $doc->findnodes( '/xs:articleList/xs:article' );
+    my @articles = ();
 
-    my @articles = ( $article1, $article2 );
+    my $length = @nodes;
+
+    foreach my $node( @nodes )
+    {
+	my $title = $node->findnodes( 'xs:title' )->get_node( 1 )->textContent;
+	my $subtitle = $node->findnodes( 'xs:subtitle' )->get_node( 1 )->textContent;
+	
+	push( @articles, ArticleList::get( $title, $subtitle ) );
+    }
 
     return Articles::get( Articles::articleList( @articles ) );
 }

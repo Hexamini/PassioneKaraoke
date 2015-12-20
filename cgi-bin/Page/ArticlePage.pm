0a90524027bd2cd0059d3d2bf8934b37acd37a60
korut94 <andreamnt94@gmail.com>
2015-12-20 20:20:22 +0100

    Lettura database per l'estrazione dei dati

--- cgi-bin/Page/ArticlePage.pm
+++ cgi-bin/Page/ArticlePage.pm
@@ -1,13 +1,34 @@
 use lib "cgi-bin";
 use strict;
+use XML::LibXML;
 
 use Page::Object::Article;
+use Page::Object::Base::ParserXML;
 
 package ArticlePage;
 
+my $file = '../data/database/articleList';
+
 sub get
 {
-    return Article::get( 'Prova', 'Potrebbe piacerti', 'Me e solo me', 'Ieri', 'Pornografico soprattutto' );
+    my( $parser, @pairs ) = @_;
+    my ( $id ) = ( ( shift @pairs ) =~ /=((\w|\d)+)$/ );
+
+    my $doc = ParserXML::getDoc( $parser, $file );
+    $article = $doc->findnodes( "//article[\@id=$id]" )->get_node( 1 );
+
+    if( $article )
+    {
+	return Article::get( $article->findnodes( 'title' )->get_node( 1 )->textContent,
+			     $article->findnodes( 'subtitle' )->get_node( 1 )->textContent,
+			     $article->findnodes( 'author' )->get_node( 1 )->textContent,
+			     $article->findnodes( 'data' )->get_node( 1 )->textContent,
+			     $article->findnodes( 'content' )->get_node( 1 )->textContent );
+    }
+    else
+    {
+	#redirect 404 page
+    }
 }
 
 1;

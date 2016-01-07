b1f2afe510c54841a97cbadd0ac4f2bc7ca2958a
Andrea Mantovani <andreamnt94@gmail.com>
2016-01-07 20:40:18 +0100

    Redirect all'userPage in caso di sessione utente presente

--- cgi-bin/Page/Page.pm
+++ cgi-bin/Page/Page.pm
@@ -17,6 +17,19 @@ sub display
     
     my $user = Session::getSession();
 
+    my $login = {
+	'user' => 'User',
+	'ref_user' => 'section=login',
+    };
+    
+    if( $user )
+    {
+	$login->{ 'user' } = '$user',
+	$login->{ 'ref_user' } = "section=userPage&user=$user",
+    }
+
+    $chain = Behavior::weld( $chain, $login );
+    
     print ParserHTML::parsing({ filename => $struct, values => $chain, });
 }
 

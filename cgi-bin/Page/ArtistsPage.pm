144432b7d6ca5b65f272be9942af52efb44e5bf1
Andrea Mantovani <andreamnt94@gmail.com>
2016-01-09 12:43:51 +0100

    Ottenimento session

--- cgi-bin/Page/ArtistsPage.pm
+++ cgi-bin/Page/ArtistsPage.pm
@@ -4,7 +4,9 @@ use XML::LibXML;
 
 use Page::Object::Artists;
 use Page::Object::ArtistsList;
+use Page::Object::EditButton;
 use Page::Object::Base::ParserXML;
+use Page::Object::Base::Session;
 
 package ArtistsPage;
 
@@ -24,6 +26,8 @@ sub get
 	push( @artists, ArtistsList::get( $name, '#' ) );
     }
 
+    my $user = Session::getSession();
+    
     return Artists::get( Artists::artistsList( @artists ) );
 }
 

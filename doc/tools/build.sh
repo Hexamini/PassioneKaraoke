2cfadde9e68b1c3e79156a25d997a269de4e9162
Davide Polonio <poloniodavide@gmail.com>
2016-01-11 15:26:28 +0100

    Removed sudo

--- doc/tools/build.sh
+++ doc/tools/build.sh
@@ -4,18 +4,18 @@
 #@license: GPLv3
 
 function install_dependencies() {
-    sudo add-apt-repository ppa:texlive-backports/ppa -y
-    sudo apt-get update -y
-    sudo apt-get install texlive-latex-extra -y 
-    sudo apt-get install texlive-luatex -y
-    sudo apt-get install texlive-fonts-extra -y
-    sudo apt-get install cm-super -y
-    sudo apt-get install texlive-bibtex-extra -y
-    sudo apt-get install latexmk -y
-    sudo apt-get install tex4ht -y
+    add-apt-repository ppa:texlive-backports/ppa -y
+    apt-get update -y
+    apt-get install texlive-latex-extra -y 
+    apt-get install texlive-luatex -y
+    apt-get install texlive-fonts-extra -y
+    apt-get install cm-super -y
+    apt-get install texlive-bibtex-extra -y
+    apt-get install latexmk -y
+    apt-get install tex4ht -y
     wget http://tug.org/fonts/getnonfreefonts/install-getnonfreefonts
-    sudo texlua install-getnonfreefonts
-    sudo getnonfreefonts-sys garamond
+    texlua install-getnonfreefonts
+    getnonfreefonts-sys garamond
 
 }
 

6fdb6af9428a11382d53166ca443efb0a3436033
Andrea Mantovani <andreamnt94@gmail.com>
2016-01-12 20:54:20 +0100

    Rimossa integrazione AdminTools con l'userPage

--- cgi-bin/Page/Object/UserPage.pm
+++ cgi-bin/Page/Object/UserPage.pm
@@ -10,20 +10,14 @@ my $struct = 'userPage';
 =Description
 Paramtri:
     username = Nickname di registrazione dell'utente
-    adminTools [ opzionale ] = Sezione amministrazione
 =cut
 sub get
 {
-    my ( $username, $adminTools ) = @_;
+    my ( $username ) = @_;
 
     my $fus = {
 	'username' => $username,
     };
-
-    if( defined $adminTools )
-    {
-	$fus = Behavior::weld( $fus, $adminTools );
-    }
 
     return Behavior::getChain( $struct, $fus );
 }

8823b8b25d25457761aedfcf27b5422b96f7e879
Andrea Mantovani <andreamnt94@gmail.com>
2016-01-14 00:58:30 +0100

    Aggiunta gestione dei likeSong

--- cgi-bin/Page/Object/UserPage.pm
+++ cgi-bin/Page/Object/UserPage.pm
@@ -1,6 +1,7 @@
 use lib "cgi-bin";
 use strict;
 
+use Page::Object::LikeSong;
 use Page::Object::Base::Behavior;
 
 package UserPage;
@@ -10,16 +11,29 @@ my $struct = 'userPage';
 =Description
 Paramtri:
     username = Nickname di registrazione dell'utente
+    likeSongs = Lista di tutte le canzoni selezionate positivamente dall'utente
 =cut
 sub get
 {
-    my ( $username ) = @_;
+    my ( $username, $likeSongs ) = @_;
 
     my $fus = {
 	'username' => $username,
+	'likeSong' => $likeSongs,
     };
 
     return Behavior::getChain( $struct, $fus );
+}
+
+sub likeSong {
+    my ( @songs ) = @_;
+    my $list = '';
+
+    foreach my $song( @songs ) {
+	$list = $list . LikeSong::extractContent( $song );
+    }
+
+    return $list;
 }
 
 1;

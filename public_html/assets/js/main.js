4918b7b6b2df58077f41e5b1f79b3758f4999ba8
Davide Polonio <poloniodavide@gmail.com>
2016-01-28 17:17:03 +0100

    Prima bozza script su controlli e pagina di test

--- public_html/assets/js/main.js
+++ public_html/assets/js/main.js
@@ -1,19 +1,30 @@
 function check(inputName, value) {
     'use strict';
 
-    var listOfInputToCheck = {
+    console.log("inputName " + inputName);
+    console.log("Sono in check");
 
-        'artistNick' : /\w+{2,}/i, //'Il nome del cantante deve avere almeno 2 caratteri composti da lettere o numeri'],
-        'artistBorn' : /\d+{1,2}\-d+{1,2}\-d+{4}/i, //'Il formato data non è valido'],
-        'artistDeath' : /\d+{1,2}\-d+{1,2}\-d+{4}/, //'La morte dell\'artista non è valida'],
-        'songTitle' : /\w+/i, //'Il titolo contiene caratteri non validi'],
-        'articleAuthor' : /\w+/i, //'L\'autore dell\'articolo non è corretto'],
-        'articleData' : /\d+{1,2}\-d+{1,2}\-d+{4}/i, //'La data inserita non è valida'],
-        'articleTitle' : /\w+/i, //'Il titolo contiene caratteri non validi'],
-        'articleSubtitle' : /\w+/i //'Il sotto-titolo contiene caratteri non validi']
+    var listOfInputToCheck = {
+        artistNick : /(\w+){2,}/, //'Il nome del cantante deve avere almeno 2 caratteri composti da lettere o numeri'],
+        artistBorn : /(\d+){1,2}\-(d+){1,2}\-(d+){4}/, //'Il formato data non è valido'],
+        artistDeath : /(\d+){1,2}\-(d+){1,2}\-(d+){4}/, //'La morte dell\'artista non è valida'],
+        songTitle : /(\w+)/, //'Il titolo contiene caratteri non validi'],
+        articleAuthor : /(\w+)/, //'L\'autore dell\'articolo non è corretto'],
+        articleData : /(\d+){1,2}\-(d+){1,2}\-(d+){4}/, //'La data inserita non è valida'],
+        articleTitle : /(\w+)/, //'Il titolo contiene caratteri non validi'],
+        articleSubtitle : /(\w+)/ //'Il sotto-titolo contiene caratteri non validi']
     };
 
-    if ( !listOfInputToCheck[inputName].test(value) ){
+
+    console.log("test2 " + inputName.toString());
+
+    var tmpVar = "\"" + inputName + "\"";
+
+    console.log(tmpVar);
+
+    if ( !listOfInputToCheck["artistNick"].test(value) ){
+
+        console.log("Cambio la voce in visible");
 
         document.getElementById("err-".concat(inputName)).className += " visible";
     }
@@ -22,16 +33,30 @@ function check(inputName, value) {
 function main() {
     'use strict';
 
+    console.log("Main chiamato");
+
     var inputTag = document.getElementsByTagName("input");
+
+    console.log(inputTag.length);
 
     for (var i = 0; i < inputTag.length; i++ ){
 
+        console.log("Sono nel for");
+
         if ( inputTag[i].getAttribute("type") === "text" ) {
 
-            inputTag[i].blur = check(inputTag[i].getAttribute("name"), inputTag[i].getAttribute("value") );
+            console.log("Stampa: " + inputTag[i].getAttribute("type"));
+
+            console.log(inputTag[i].getAttribute("name"));
+
+            //inputTag[i].onblur = check(inputTag[i].getAttribute("name"), inputTag[i].getAttribute("value") );
+            inputTag[i].addEventListener("blur", check.bind(null, inputTag[i].getAttribute("name"), inputTag[i].getAttribute("value")))
+
         }
     }
 
 }
 
 window.onload = main();
+
+//window.document.getElementsByTagName("body").onload = main();

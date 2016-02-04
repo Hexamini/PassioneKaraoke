b2ab54d7ef0f924a96345603762daf04e5f32d99
Andrea Mantovani <andreamnt94@gmail.com>
2016-02-04 22:31:07 +0100

    Aggiunto label per articoli

--- cgi-bin/Page/MapPage.pm
+++ cgi-bin/Page/MapPage.pm
@@ -96,7 +96,7 @@ sub get {
 
 	push @childrenArticle, MapList::get(
 	    Link::get(
-		"$title - $subtitle",
+		"Articolo: $title - $subtitle",
 		"r.cgi?section=article&id=$idArticle"
 	    )
 	);
